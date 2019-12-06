ea783e3223670ea6a74e746219052ec1eb07fb4b
Richard Bruskiewich <richard.bruskiewich@delphinai.com>
2019-12-06 20:53:21 +0000

    setup update

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name="translator-knowledge-beacon",
-    url="https://github.com/NCATS-Tangerine/translator-knowledge-beacon",
-    version = "1.3.0",
-    packages = [
+    name="translator-knowledge-graph-beacon",
+    url="https://github.com/NCATS-Tangerine/tkg-beacon",
+    version="1.3.1",
+    packages=[
         'beacon_controller',
         'beacon_controller.controllers',
         'beacon_controller.database',
@@ -13,18 +13,18 @@ setup(
     ],
     include_package_data=True,
     install_requires=[
-        'BiolinkMG',
+        'bmt',
+        'biolinkml',
         'neomodel',
         'pandas',
         'cachetools',
         'tornado',
-        'requests',
+        'requests >= 2.22',
         'flask',
         'pyyaml',
         'connexion == 1.1.15',
-        'python_dateutil',
+        'python_dateutil == 2.6.1',
         'setuptools >= 21.0.0',
         'prefixcommons',
-        'bmt',
     ]
 )
